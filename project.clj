74f3484c96bd3039128e1df4c13894182cb407a6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-23 21:08:32 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "A simple API to expose some properties file."
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.8"]
+                 [clojurewerkz/propertied "1.2.0"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler webproperty.handler/app}
   :profiles
