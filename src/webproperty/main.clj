2d5a0f99948d13cfe3957abaa808334c6d3ce8b4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-24 12:39:01 +0200

    Refactor - Simplify the main namespace

--- src/webproperty/main.clj
+++ src/webproperty/main.clj
@@ -1,29 +1,18 @@
 (ns webproperty.main
   "Main namespace to trigger the application."
   (:require [webproperty.handler :refer [app]]
+            [compojure.handler :as handler]
             [clojure.java.io :as io]
-            [ring.util.response :as resp]
             [ring.middleware.stacktrace :as trace]
             [ring.middleware.session :as session]
             [ring.middleware.session.cookie :as cookie]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.basic-authentication :as basic]
-            [cemerick.drawbridge :as drawbridge]
             [environ.core :refer [env]]))
 
 (defn- authenticated? [user pass]
   ;; TODO: heroku config:add REPL_USER=[...] REPL_PASSWORD=[...]
   (= [user pass] [(env :repl-user false) (env :repl-password false)]))
-
-(def ^:private drawbridge
-  (-> (drawbridge/ring-handler)
-      (session/wrap-session)
-      (basic/wrap-basic-authentication authenticated?)))
-
-(defn response [content-type body]
-  "Generic get response"
-  (-> (resp/response body)
-      (resp/content-type content-type)))
 
 (defn wrap-error-page [handler]
   (fn [req]
@@ -34,13 +23,12 @@
             :body (slurp (io/resource "500.html"))}))))
 
 (defn -main [& [port]]
-  (let [port (Integer. (or port (env :port) 5000))
-        store (cookie/cookie-store {:key (env :session-secret)})]
+  (let [port (Integer. (or port (env :port) 5000))]
     (jetty/run-jetty (-> #'app
                          ((if (env :production)
                             wrap-error-page
                             trace/wrap-stacktrace))
-                         (site {:session {:store store}}))
+                         handler/site)
                      {:port port :join? false})))
 
 ;; For interactive development:
