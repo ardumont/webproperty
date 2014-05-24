0878e8bb8025dd092b150fcab258534b4348f673
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-24 12:54:03 +0200

    Add the means to access a property key

--- src/webproperty/handler.clj
+++ src/webproperty/handler.clj
@@ -7,16 +7,28 @@
             [webproperty.properties :as properties]
             [webproperty.config :as config]))
 
-(defn response [content-type body]
+(defn- response [content-type body]
   "Generic get response"
   (-> (resp/response body)
       (resp/content-type content-type)))
 
+(defn- load-map-from-properties [filename]
+  "Load properties from the filename."
+  (->> filename
+       (format "%s/%s.properties" config/webproperty-properties-folder)
+       properties/load-properties-file))
+
 (defroutes app-routes
   (GET "/" [] "An API to manipulate properties file.")
-  (GET "/properties/:filename" [filename :as req]
-       (->> (format "%s/%s.properties" config/webproperty-properties-folder filename)
-            properties/load-properties-file
+  (GET "/properties/:filename" [filename]
+       (->> filename
+            load-map-from-properties
+            pr-str
+            (response "text/plain")))
+  (GET "/properties/:filename/:key" [filename key]
+       (->> filename
+            load-map-from-properties
+            (get key)
             pr-str
             (response "text/plain")))
   (route/resources "/")
