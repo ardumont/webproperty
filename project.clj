a46240f4dfb7682b14147d19f63f1daecb1497f9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-28 16:19:57 +0200

    Add clj-http deps to ease manual testing

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [clojurewerkz/propertied "1.2.0"]
                  [ring/ring-jetty-adapter "1.2.1"]
                  [ring/ring-devel "1.2.1"]
-                 [environ "0.4.0"]]
+                 [environ "0.4.0"]
+                 [clj-http "0.9.2"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler webproperty.handler/app}
   :profiles
