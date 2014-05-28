25eff9e1dd324817b55a1196ef9889f49cbc729c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-28 16:34:34 +0200

    Refactor - align deps

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject webproperty "0.1.0-SNAPSHOT"
   :description "A simple API to expose some properties file."
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]
+  :dependencies [[org.clojure/clojure     "1.6.0"]
+                 [compojure               "1.1.8"]
                  [clojurewerkz/propertied "1.2.0"]
                  [ring/ring-jetty-adapter "1.2.1"]
-                 [ring/ring-devel "1.2.1"]
-                 [environ "0.4.0"]
-                 [clj-http "0.9.2"]
-                 [org.clojure/data.json     "0.2.4"]]
+                 [ring/ring-devel         "1.2.1"]
+                 [environ                 "0.4.0"]
+                 [clj-http                "0.9.2"]
+                 [org.clojure/data.json   "0.2.4"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler webproperty.handler/app}
   :profiles
