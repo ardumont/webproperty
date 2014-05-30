7b4463036f4454252195aa6399c734ab7d74e9f2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-30 16:48:08 +0200

    Use environ to retrieve the property variable

--- src/webproperty/config.clj
+++ src/webproperty/config.clj
@@ -3,7 +3,8 @@
 The application is setuped through a property file referenced by default in /tmp/bootstrap-webproperty.properties.
 This bootstrap configuration file can be overriden by a jvm property bootstrap-webproperty.
 `-Dbootstrap-webproperty=/some/path/to/bootstrap/properties/file.`"
-  (:require [webproperty.properties :refer [load-properties-file]]))
+  (:require [webproperty.properties :refer [load-properties-file]]
+            [environ.core :refer [env]]))
 
 ;; Retrieve the folder that exposes the properties files
 (def webproperty-properties-folder (let [;; the default filepath to the bootstrap webproperty configuration file
@@ -11,7 +12,7 @@ This bootstrap configuration file can be overriden by a jvm property bootstrap-w
                                          ;; Determine the path to the bootstrap webproperty configuration file
                                          ;; If a jvm property exists referencing it, we use it
                                          ;; otherwise, we default to the value of `default-webproperty-filepath`
-                                         bootstrap-webproperty-filepath (if-let [property-filepath (System/getProperty "bootstrap-webproperty")]
+                                         bootstrap-webproperty-filepath (if-let [property-filepath (env :bootstrap-webproperty)]
                                                                           property-filepath
                                                                           default-webproperty-filepath)
                                          ;; Load the bootstrap configuration file from the system
