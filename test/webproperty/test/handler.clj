f72191d8762bfd374a80b2ceb64d9bbe94b52d57
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-31 11:39:31 +0200

    Fix tests

--- test/webproperty/test/handler.clj
+++ test/webproperty/test/handler.clj
@@ -1,14 +1,14 @@
 (ns webproperty.test.handler
   (:use clojure.test
-        ring.mock.request  
+        ring.mock.request
         webproperty.handler))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
+      (is (= (:body response) "an API to manipulate properties file."))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
