d2c73c619b6ad305006d76689111de061865704f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-31 13:09:12 +0200

    Add test about internal function computation

--- test/webproperty/test/handler.clj
+++ test/webproperty/test/handler.clj
@@ -1,7 +1,9 @@
 (ns webproperty.test.handler
   (:use clojure.test
         ring.mock.request
-        webproperty.handler))
+        webproperty.handler)
+  (:require [midje.sweet :refer :all]
+            [webproperty.properties :as config]))
 
 (deftest test-app
   (testing "main route"
@@ -12,3 +14,11 @@
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+(fact "Compute the filepath given a filename"
+  (filepath-from-filename "/some/file/path/to" "some-name") => "/some/file/path/to/some-name.properties")
+
+(fact "Load map from properties"
+  (load-map-from-properties "/some/file/path/to" "some-name")  => :some-map
+  (provided
+    (config/load-properties-file "/some/file/path/to/some-name.properties") => :some-map))
