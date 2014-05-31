d58bf00d642dde14b1def5d8f4af58c4640f573a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-31 10:48:41 +0200

    Update environ deps to the latest

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [clojurewerkz/propertied "1.2.0"]
                  [ring/ring-jetty-adapter "1.2.1"]
                  [ring/ring-devel         "1.2.1"]
-                 [environ                 "0.4.0"]
+                 [environ                 "0.5.0"]
                  [clj-http                "0.9.2"]
                  [org.clojure/data.json   "0.2.4"]]
   :plugins [[lein-ring "0.8.10"]]
